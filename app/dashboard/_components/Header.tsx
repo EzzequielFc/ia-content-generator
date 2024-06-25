faf5741dd8d84334933f29d12eefc3d02baf3bc7
Ezequiel Campos <ezequielcampos@Air-de-Ezequiel.local>
2024-06-25 14:33:33 -0300

    feat: dashboard layout

---
+++ app/dashboard/_components/Header.tsx
@@ -0,0 +1,21 @@
+import { Search } from "lucide-react";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between p-5 shadow-sm border-b-2 items-center">
+      <div className="flex gap-2 items-center p-2 border rounded-md max-w-md">
+        <Search />
+        <input type="text" placeholder="Search..." className="outline-none" />
+      </div>
+
+      <div>
+        <h2 className="bg-primary text-white rounded-full px-2 text-sm">
+          🔥 Join Membership just for $9.99/Month
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
