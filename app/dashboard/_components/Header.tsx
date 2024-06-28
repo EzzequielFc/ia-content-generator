001f3f3dcf4841691f4e1956d2b768d9d6c00043
EzzequielFc <contato.ezequielf@gmail.com>
2024-06-28 13:46:33 -0300

    dynamic form display

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="flex justify-between p-5 shadow-sm border-b-2 items-center">
-      <div className="flex gap-2 items-center p-2 border rounded-md max-w-md">
+    <div className="flex justify-between p-5 shadow-sm border-b-2 items-center bg-white">
+      <div className="flex gap-2 items-center p-2 border rounded-md max-w-md bg-white">
         <Search />
         <input type="text" placeholder="Search..." className="outline-none" />
       </div>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ function layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="bg-purple-50 h-screen">
       <div className="md:w-64 hidden md:block fixed">
         <SideNav />
       </div>
